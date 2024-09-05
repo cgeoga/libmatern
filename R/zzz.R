9189ffd87c2c0fe2a87bb6c14d00c21e24e41763
gmaney17 <137039269+gmaney17@users.noreply.github.com>
2024-09-05 01:42:40 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -15,11 +15,11 @@ download_artifact <- function(pkgname) {
 	status <- NULL
 
 	if (os_type == "Linux") {
-		status <- system(paste("curl -L", linux_url, " > libmatern.so.zip"))
+		status <- system(paste("curl -L ", linux_url, " > libmatern.so.zip"))
 		system("unzip libmatern.so.zip && mv libmatern.so libs")
 		lib_file <- system.file("libs", "libmatern.so", package = pkgname)
 	} else if (os_type == "Darwin") {
-		status <- system(paste("curl -L", mac_url, " > libmatern.dylib.zip"))
+		status <- system(paste("curl -L ", mac_url, " > libmatern.dylib.zip"))
 		system("unzip libmatern.dylib.zip && mv libmatern.dylib libs")
 		lib_file <- system.file("libs", "libmatern.dylib", package = pkgname)
 	} else if (os_type == "Windows") {
