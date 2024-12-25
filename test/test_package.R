b71be66ddd1a71072085234abb84fbf2c5818b6b
gmaney17 <137039269+gmaney17@users.noreply.github.com>
2024-12-24 19:25:45 -0500

    Update test_package.R

--- test/test_package.R
+++ test/test_package.R
@@ -1,4 +1,4 @@
 install.packages("remotes")
-remotes::install_github("cgeoga/libmatern")
+remotes::install_github("cgeoga/libmatern", auth_token = Sys.getenv("GITHUB_PAT"))
 library("libmatern")
 besselk(1.1, 2.1, 0)
